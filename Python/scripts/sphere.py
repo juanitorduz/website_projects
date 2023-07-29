9479a0ff5629a583e172409d7e729392856bce7a
juanitorduz <juanitorduz@gmail.com>
2023-07-29 22:11:47 +0200

    add sphere.py

---
+++ Python/scripts/sphere.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+fig = plt.figure(figsize=(15, 15))
+ax = fig.add_subplot(projection="3d")
+
+u, v = np.mgrid[0 : 2 * np.pi : 100j, 0 : np.pi : 100j]
+x = np.cos(u) * np.sin(v)
+y = np.sin(u) * np.sin(v)
+z = np.cos(v)
+ax.view_init(elev=30, azim=30)
+ax.plot_surface(x, y, z, cmap="Spectral", rstride=3, cstride=3, edgecolor="k")
+ax.set_box_aspect((1, 1, 0.87))
+ax.grid(False)
+ax.axis("off")
+plt.savefig("sphere_spectral.png", dpi=500, bbox_inches="tight")
